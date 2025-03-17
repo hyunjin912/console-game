784cc2581f317a49feb89dfc10b2cca79f7715c3
LeeHyunJin <skyhyunjinlee@gmail.com>
2025-03-17 21:31:52 +0900

    Feat: 게임 클래스 작성
    속성, 메서드 정의

--- lib/Game.dart
+++ lib/Game.dart
@@ -1,1 +1,16 @@
-class Game {}
+import 'package:console_game/Character.dart';
+import 'package:console_game/Monster.dart';
+
+class Game {
+  Character character;
+  List<Monster> monsters;
+  int huntingCount = 0;
+
+  Game() {}
+
+  void startGame() {}
+
+  void battle() {}
+
+  void getRandomMonster() {}
+}
