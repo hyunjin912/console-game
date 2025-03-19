22a42d10d7de03125e8ca63e5bb19436c9ebdd44
LeeHyunJin <skyhyunjinlee@gmail.com>
2025-03-19 14:42:45 +0900

    Feat: 공격과 저장 기능 완료
    공격을 통해 새롭게 몬스터를 불러오는 과정과
    결과를 저장하는 기능을 작성

--- lib/Character.dart
+++ lib/Character.dart
@@ -1,29 +1,33 @@
 import 'package:console_game/Monster.dart';
 
+/// 캐릭터를 만들기 위한 클래스
 class Character {
-  String name;
+  String? name;
   int hp;
   int atk;
   int def;
 
-  Character({
-    required this.name,
-    required this.hp,
-    required this.atk,
-    required this.def,
-  });
+  Character({required this.hp, required this.atk, required this.def});
 
+  /// 몬스터를 공격하는 메서드
   void attackMonster(Monster monster) {
     monster.hp -= atk;
     print('$name이(가) ${monster.name}에게 $atk의 데미지를 입혔습니다.');
+
+    if (monster.hp == 0) {
+      print('${monster.name}을(를) 물리쳤습니다!');
+    }
   }
 
+  /// 몬스터의 공격으로부터 방어를 하기 위한 메서드
   void defend() {}
 
+  /// 행동(턴)이 끝난 후 캐릭터의 상태를 보여주기 위한 메서드
   void showStatus() {
     print('$name - 체력: $hp, 공격력: $atk, 방어력: $def');
   }
 
+  // print()로 캐릭터의 속성을 보기 위해 오버라이딩
   @override
   String toString() {
     return 'Character {name: $name, hp: $hp, atk: $atk, def: $def}';
--- lib/Game.dart
+++ lib/Game.dart
@@ -7,18 +7,17 @@ import 'package:console_game/file_utils.dart';
 
 class Game {
   late Character character;
-  List<String> characterInitialValue = [];
   List<Monster> monsters = [];
   late Monster currentMonster;
   int huntingCount = 0;
   late String characterName;
 
+  // 생성자. 캐릭터와 몬스터의 데이터를 가져오며 해당되는 인스턴스 변수에 값을 초기화 한다.
   Game() {
     String characterData = getData('lib/characters.txt');
     String monsterData = getData('lib/monsters.txt');
     RegExp monsterRegexp = RegExp(r'^[a-zA-Z]+,\d+,\d+$', multiLine: true);
-    characterInitialValue = characterData.split(',');
-
+    List<String> characterInitialValue = characterData.split(',');
     List<RegExpMatch> mInfo = monsterRegexp.allMatches(monsterData).toList();
 
     for (var i = 0; i < mInfo.length; i++) {
@@ -33,73 +32,108 @@ class Game {
         ),
       );
     }
+
+    character = Character(
+      hp: int.parse(characterInitialValue[0]),
+      atk: int.parse(characterInitialValue[1]),
+      def: int.parse(characterInitialValue[2]),
+    );
   }
 
   void startGame() {
-    // bool isStart = true;
-    RegExp nameRegexp = RegExp(r'^[a-zA-Z]+$');
-
-    // while (isStart) {
     stdout.write('\n캐릭터의 이름을 영어로 입력하세요: ');
     String characterName = stdin.readLineSync()!;
+    RegExp nameRegexp = RegExp(r'^[a-zA-Z]+$');
 
     try {
       if (!nameRegexp.hasMatch(characterName)) {
         throw Customexception('\n캐릭터의 이름이 영어가 아니므로 재시작합니다.');
       }
 
-      character = Character(
-        name: characterName,
-        hp: int.parse(characterInitialValue[0]),
-        atk: int.parse(characterInitialValue[1]),
-        def: int.parse(characterInitialValue[2]),
-      );
-
+      character.name = characterName;
       currentMonster = getRandomMonster();
 
       print('\n게임을 시작합니다!');
       character.showStatus();
-      print('새로운 몬스터가 나타났습니다!');
+      print('\n새로운 몬스터가 나타났습니다!');
       currentMonster.showStatus();
 
-      battle(); // 이 메서드에 while로 랩핑하면 battle내에서 에러가 뜨면 지금 여기의 catch문이 동작해서 startGame이 다시 시작되므로 하면 안될 듯
+      battle();
     } catch (e) {
       print(e);
       startGame();
     }
-    // }
   }
 
   void battle() {
-    try {
-      print('${character.name}의 턴');
+    bool isBattle = true;
+    String nextStage;
+    String resultStage;
 
-      stdout.write('행동을 선택하세요 (1: 공격, 2: 방어): ');
-      String actionNumber = stdin.readLineSync()!;
+    while (isBattle) {
+      try {
+        if (currentMonster.hp == 0) {
+          if (monsters.isEmpty) {
+            print('\n축하합니다! 모든 몬스터를 물리쳤습니다.');
+            stdout.write('모험을 저장하시겠습니까? (y/n): ');
 
-      switch (actionNumber) {
-        case '1':
-          print('\n여기는 1번입니다.\n');
-          character.attackMonster(currentMonster);
-          break;
-        case '2':
-          print('${character.name}이(가) 방어 태세를 취하여 0 만큼 체력을 얻었습니다.\n');
-          break;
-        default:
-          throw Customexception('\n일치하는 행동이 없습니다.\n');
+            resultStage = stdin.readLineSync()!;
+            switch (resultStage) {
+              case 'y':
+                createData('${character.name}, ${character.hp}, 승리');
+                break;
+              case 'n':
+                print('\n${character.name}의 모험이 끝났습니다.');
+                print('비록, 모험의 기록은 남지 않더라도 당신의 모험은 구전될 것입니다.');
+                break;
+              default:
+                throw Customexception('\n${character.name}의 의지를 정확히 알려주세요.');
+            }
+            return;
+          }
+
+          stdout.write('\n다음 몬스터와 싸우시겠습니까? (y/n): ');
+          nextStage = stdin.readLineSync()!;
+
+          switch (nextStage) {
+            case 'y':
+              currentMonster = getRandomMonster();
+              print('\n새로운 몬스터가 나타났습니다!');
+              currentMonster.showStatus();
+              break;
+            case 'n':
+              print('\n${character.name}의 모험이 끝났습니다.');
+              return;
+            default:
+              throw Customexception('\n${character.name}의 의지를 정확히 알려주세요.');
+          }
+        }
+
+        print('\n${character.name}의 턴');
+        stdout.write('행동을 선택하세요 (1: 공격, 2: 방어): ');
+        String actionNumber = stdin.readLineSync()!;
+
+        switch (actionNumber) {
+          case '1':
+            character.attackMonster(currentMonster);
+            if (currentMonster.hp == 0) continue;
+            break;
+          case '2':
+            print('${character.name}이(가) 방어 태세를 취하여 0 만큼 체력을 얻었습니다.');
+            break;
+          default:
+            throw Customexception('\n일치하는 행동이 없습니다.\n');
+        }
+
+        print('\n${currentMonster.name}의 턴');
+        currentMonster.attackCharacter(character);
+        print('\n[ 진행상황 ]');
+        character.showStatus();
+        currentMonster.showStatus();
+        print('\n-----------------------------------');
+      } catch (e) {
+        print(e);
       }
-
-      print('\n${currentMonster.name}의 턴');
-      currentMonster.attackCharacter(character);
-      print('[ 진행상황 ]');
-      character.showStatus();
-      currentMonster.showStatus();
-      print('-----------------------------------');
-
-      battle();
-    } catch (e) {
-      print(e);
-      battle();
     }
   }
 
